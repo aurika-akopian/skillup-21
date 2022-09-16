88ffab3902154fe416e76b5acc39aeb892e219fa
Autika Akopian <aurika.akopyan56@gmail.com>
2022-09-16 19:08:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('button').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
+  });
+
+  $('a').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $("#elem").offset().top
+    }, 500);
+    return false;
+  });
