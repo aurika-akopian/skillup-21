371665eb3584f22562a8950994635bc2e47c3e66
Autika Akopian <aurika.akopyan56@gmail.com>
2022-10-15 16:04:05 +0200

    fix commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 $('button').on('click', function () {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 500);
-  });
+  $('html, body').animate({
+    scrollTop: 0
+  }, 500);
+});
 
-  $('a').on('click', function () {
-    $('html, body').animate({
-      scrollTop: $("#elem").offset().top
-    }, 500);
-    return false;
-  });
-
-
+$('a').on('click', function () {
+  $('html, body').animate({
+    scrollTop: $("#elem").offset().top
+  }, 500);
+  return false;
+});
